4c476c5092f4a6359c125ea001a56afecc7652f9
vagner.olliver27 <vagner.oliveira@terceiros.zenvia.com>
2019-02-24 16:06:51 -0300

    test api

--- tests/unit/api-terms.spec.js
+++ tests/unit/api-terms.spec.js
@@ -1,22 +1,18 @@
-import { getTerms, getTerm, saveTerm, persistTerm, deleteTerm } from "@/untils/crud";
+import { getTerm, getTerms, saveTerm, persistTerm, deleteTerm } from "@/untils/crud";
 
 describe('Component', () => {
+  let newTerm;
 
-  afterEach(() => {
-    deleteTerm('1', 'termsTest')
+  beforeEach( async () => {
+    const term = { "name": "label_cancel", "value": "cancelar" }
+    newTerm = await saveTerm(term, 'termsTest')
   });
 
-  test('Get term', () => {
-    return getTerms('termsTest')
-      .then(response => {
-        expect(typeof response).toEqual('object');
-      })
+  afterEach( async () => {
+    newTerm = await deleteTerm(newTerm.id, 'termsTest')
   });
 
   test('should persist one term save', async () => {
-    const term = { "name": "label_cancel", "value": "cancelar" }
-    const newTerm = await saveTerm(term, 'termsTest')
-
     const updateTerm = { value: 'save and continue'}
     const params = Object.assign(newTerm, updateTerm);
 
@@ -26,16 +22,20 @@ describe('Component', () => {
        })
   });
 
-  test('should term by id', async () => {
-    const term = { "name": "label_cancel", "value": "cancelar" }
-    const newTerm = await saveTerm(term, 'termsTest')
-
+  test('should get term by id', async () => {
     return getTerm(newTerm.id, 'termsTest')
       .then(response => {
         expect(typeof response).toEqual('object');
         expect(response.id).toEqual(newTerm.id)
         expect(response.name).toEqual('label_cancel')
         expect(response.value).toEqual('cancelar')
+      })
+  });
+
+  test('should get terms', async () => {
+    return getTerms('termsTest')
+      .then(response => {
+        expect(response[0].id).toEqual(newTerm.id)
       })
   });
 })
